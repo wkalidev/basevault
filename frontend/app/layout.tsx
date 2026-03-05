aff23bec7027873e88386e0e2edd124ae12c0117
wkalidev <wkalidev@gmail.com>
2026-03-05 19:43:59 +0100

    modified: frontend/app/layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -7,6 +7,13 @@ export const metadata: Metadata = {
   description: 'ERC-4626 yield vault on Base',
 };
 
+// Talent app project verification
+other:  {
+  "talentapp:project_verification" ; "47d55103b518dc0ea79085c69e1da15233fc3da75490f9dabcd5c4b9b68b4d644cee722a17aa985a5266669055c049b28c3bea56b7068fbb735291e7b5bd0cc2"
+//  'apple-mobile-web-app-capable': 'yes',
+//  'apple-mobile-web-app-status-bar-style': 'black-translucent',
+};
+
 export default function RootLayout({
   children,
 }: {
